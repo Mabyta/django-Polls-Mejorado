7fc0ab665f2c9189bada0e2fdc1198864b820c08
axelv <axel.valenzuela@uabc.edu.mx>
2018-07-13 19:04:42 +0000

    Se creo navbar, se agrego cosas a account y se hicieron tests

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@ from accounts import views as accounts_views
 
 
 urlpatterns = [
-    path('signup/', accounts_views.signup, name='signup'),
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', accounts_views.signup, name='signup'),# Se llama = {% url 'signup' %}
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),# Se llama = {% url 'login' %}
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),# Se llama = {% url 'loguot' %}
 ]
--- super_polls/urls.py
+++ super_polls/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from super_polls.views import encuestas_view,resultados_view,inicio_view
 
 urlpatterns = [
-    path('inicio/', inicio_view, name='inicio'),
-    path('encuestas/', encuestas_view),
-    path('resultados/', resultados_view),
+    path('', inicio_view, name='inicio'),
+    path('Home/', inicio_view, name='home'),# Se llama = {% url 'home' %} y me lo pide obligatoriamente accounts >:(
+    path('inicio/', inicio_view, name='inicio'),# Se llama = {% url 'inicio' %}
+    path('encuestas/', encuestas_view, name='encuestas'),# Se llama = {% url 'encuestas' %}
+    path('resultados/', resultados_view, name='resultados'),# Se llama = {% url 'resultados' %}   :)  >u<   UNA VEZ MAS... Tramposa!! Buajajaja
 ]
